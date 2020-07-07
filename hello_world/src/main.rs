1bc62861415f7c49d17990cc9a4e60581a92f08f
arme <arme.tr@gmail.com>
2020-07-08 01:50:49 +0700

    refactor

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,22 +1,23 @@
 fn main() {
-    let mut _flip = true;
+    let mut flip = true;
     let mut total = 0.;
-    for x in (1..1003).step_by(2) {
-        let n = x.to_string().chars().count() as u32;
+    for i in (1..1003).step_by(2) {
+        let x = i as f64;
+        let n = i.to_string().chars().count() as u32;
         let e = if n == 1 { 1f64 } else { (10u32.pow(n - 1)) as f64 };
-        if _flip {
-            let p = (4f64 * e) / (x as f64);
+        if flip {
+            let p = (4f64 * e) / x;
             print!("({}/{}) => ", (4f64 * &e), x);
             println!("+{:.}", p);
             total += p;
         } else {
-            let p = (4f64 * e) / (x as f64);
+            let p = (4f64 * e) / x;
             print!("({}/{}) => ", (4f64 * &e), x);
             println!("-{:.}", p);
             let inv_p = -p;
             total += inv_p;
         }
-        _flip = !_flip;
+        flip = !flip;
     }
     println!("PI = {:.}", total);
     //println!("{:.}", 0.0000004004004004004004f64);
