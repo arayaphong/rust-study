49136216d131eca4da92be4ee117ebd008a5d158
arme <arme.tr@gmail.com>
2020-07-08 02:04:19 +0700

    refactor

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,18 +4,15 @@ fn main() {
     for i in (1..1003).step_by(2) {
         let x = i as f64;
         let n = i.to_string().chars().count() as u32;
-        let e = if n == 1 { 1f64 } else { (10u32.pow(n - 1)) as f64 };
+        let e = 10u32.pow(n - 1) as f64;
+        let p = (4f64 * e) / x;
+        print!("({}/{}) => ", (4f64 * &e), x);
         if flip {
-            let p = (4f64 * e) / x;
-            print!("({}/{}) => ", (4f64 * &e), x);
             println!("+{:.}", p);
             total += p;
         } else {
-            let p = (4f64 * e) / x;
-            print!("({}/{}) => ", (4f64 * &e), x);
             println!("-{:.}", p);
-            let inv_p = -p;
-            total += inv_p;
+            total += -p;
         }
         flip = !flip;
     }
