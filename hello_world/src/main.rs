56e8c67b169515990f2c5a64ee73077003561a11
arayaphong <arme.tr@gmail.com>
2020-07-07 19:18:16 +0700

    Add files via upload

---
+++ hello_world/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut _flip = true;
+    let mut total = 0.;
+    for x in (1..9000000).step_by(2) {
+        let p = 4.0 / x as f64;
+        if _flip {
+            println!("{}", p);
+            total = total + p;
+        } else {
+            println!("{}", p);
+            let inv_p = -p;
+            total += inv_p;
+        }
+        _flip = !_flip;
+    }
+    println!("PI = {:.}", total);
+}
