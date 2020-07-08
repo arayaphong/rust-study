ab45bd880e51d5f0656ce3b9bdb0d440d479db9d
arme <arme.tr@gmail.com>
2020-07-08 13:39:30 +0700

    fill head zero

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,13 +6,19 @@ fn main() {
         let n = i.to_string().chars().count() as u32;
         let e = 10u32.pow(n - 1) as f64;
         let p = (4f64 * e) / x;
+        let str_p = p.to_string().replace('.', "");
+        let arr = vec!['0'; (n - 1) as usize];
+        let s1: String = arr.iter().collect();
+        let mut s2 = s1 + &str_p;
+        s2.insert(1, '.');
+        let p2: f64 = s2.parse().unwrap();
         print!("({}/{}) => ", (4f64 * &e), x);
         if flip {
-            println!("+{:.}", p);
-            total += p;
+            println!("+{:.}", p2);
+            total += p2;
         } else {
-            println!("-{:.}", p);
-            total += -p;
+            println!("-{:.}", p2);
+            total += -p2;
         }
         flip = !flip;
     }
